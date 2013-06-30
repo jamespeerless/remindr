a65d1edd70f78c6fdf9e7ce63f3dd66e0e34a993
James Peerless <jamespeerless@gmail.com>
2013-06-30 10:23:32 -0400

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Remindr::Application.config.secret_key_base = '1b9378a6f9413f109b7ac53ad96a65ee84467cfa95dcf884d43196a2155ca928dc20e7f920e90df07d145a18ad59286371ade0eee216edc7460612b3913c5687'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Remindr::Application.config.session_store :cookie_store, key: '_remindr_session'
